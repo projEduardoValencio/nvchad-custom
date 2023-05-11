ccfbf113c63b1e3ea47ea536b753b7dda638c95f
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2023-05-11 10:48:58 -0300

    fix: formatter keymap

--- mappings.lua
+++ mappings.lua
@@ -4,7 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ['<F3>'] = { ":lua vim.lsp.buf.format()", opts = { nowait = true}}
+    ["<F3>"] = { ":lua vim.lsp.buf.format()<CR>", opts = { nowait = true } },
   },
 }
 
